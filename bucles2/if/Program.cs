843fca4c0c69ceb15a45575852c55bb4e6aa8771
snipgamer1 <chuchis619@hotmail.com>
2023-05-21 21:21:03 -0600

    Bucles 2

---
+++ bucles2/if/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Introduce nombre: ");
+string name = Console.ReadLine();
+Console.Write("Introduce email: ");
+string email = Console.ReadLine();
+Console.Write("Introduce codigo del cupon (o 'none' si no tiene): ");
+string coupon = Console.ReadLine();
+
+double price = 100; // fixed price of product
+double discount = 0;
+
+if (coupon == "SAVE10")
+{
+    discount = 0.1;
+}
+else 
+{
+    discount = 0;
+}
+
+double finalPrice = price - (price * discount);
+Console.WriteLine("Final price: {0}", finalPrice);
+        
---
+++ bucles2/switch/Program.cs
@@ -0,0 +1,28 @@
+﻿List<string> languages = new List<string> { "C#", "Java", "C++" };
+Console.WriteLine("Selecciona un lenguaje:");
+for (int i = 0; i < languages.Count; i++)
+{
+    Console.WriteLine("{0}. {1}", i + 1, languages[i]);
+}
+
+string input = Console.ReadLine();
+string selectedLanguage;
+switch (input)
+{
+    case "1":
+    case "a":
+        selectedLanguage = languages[0];
+        break;
+    case "2":
+    case "b":
+        selectedLanguage = languages[1];
+        break;
+    case "3":
+    case "c":
+        selectedLanguage = languages[2];
+        break;
+    default:
+        selectedLanguage = "Invalid selection";
+        break;
+}
+Console.WriteLine("Lenguaje Seleccionado: {0}", selectedLanguage);
